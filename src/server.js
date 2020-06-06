1660f6c71ca022b805304b0e42cbd499b9c483f4
alancsoliveira <alancristian@alu.ufc.br>
2020-06-05 23:41:48 -0300

    Refatorando o código

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const server = express();
+
+
+server.use(express.static("public"));
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+});
+
+server.get("/", (req, res) => {
+  return res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html")
+});
+
+server.get("/search", (req, res) => {
+  return res.render("search-results.html")
+});
+
+
+server.listen(3000);
+
+
+// 1:20:00
